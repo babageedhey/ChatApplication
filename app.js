bda6a6fed1a36b9194cbf9670e82bcde910d5040
babageedhey <babageedhey@gmail.com>
2018-08-31 13:58:30 +0100

    initial deployment

---
+++ app.js
@@ -0,0 +1,46 @@
+const express       = require('express');
+
+const app   = express();
+
+
+
+//SEtting the template Engine
+app.set('view engine', 'ejs')
+
+//Middleware
+app.use(express.static('public'))
+
+
+//ROutes
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+
+
+//Port Listening
+server              = app.listen(3000)
+const io            = require('socket.io')(server)
+
+//SocketIO Listens on every connection
+io.on('connection', (socket) =>{
+    console.log('New user connected')
+
+    //default username
+    socket.username = "Anonymous"
+
+    //listen to change of username by User
+    socket.on('change_username', (data) =>{
+        socket.username = data.username
+    })
+
+    //Listen for new messages
+    socket.on('new_message', (data) =>{
+        io.sockets.emit('new_message', {message : data.message, username: socket.username});
+    })
+
+    //Listen on typing
+    socket.on('typing', (data) =>{
+        socket.broadcast.emit('typing', {username: socket.username})
+    })
+})
---
+++ public/chat.js
@@ -0,0 +1,41 @@
+$(function(){
+    var socket = io.connect('http://localhost:3000')
+
+    //buttons and inputs
+    var message         = $("#message")
+    var username        = $("#username")
+    var send_message    = $("#send_message")
+    var send_username   = $("#send_username")
+    var chatroom        = $("#chatroom")
+    var feedback        = $("#feedback")
+
+    //Emit Message
+    send_message.click(function(){
+        socket.emit('new_message', {message: message.val()})
+    })
+
+    //Listen for New message
+    socket.on("new_message", (data) =>{
+        console.log(data);
+        feedback.html('');
+        message.val('');
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+
+    //Emit Username
+    send_username.click(function() {
+        console.log(username.val())
+        socket.emit('change_username', {username: username.val()})
+    })
+
+    //Emit Typing
+    message.bind("keypress", () =>{
+        socket.emit('typing')
+    })
+
+    //Listening on Typing
+    socket.on('typing', (data) =>{
+        feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+    })
+});
